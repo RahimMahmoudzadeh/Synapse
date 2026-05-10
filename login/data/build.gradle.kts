6c227b20e71c7d32568536ccbded4bfddf55c5b5
Rahim Mahmoudzadeh <rahim.mahmoudzadehh@gmail.com>
2026-05-10 11:41:13 +0330

    Refactor "login:data" module and upgrade project JDK version
    - Convert `login:data` from a Kotlin JVM library to an Android library module.
    - Apply `library` and `di` convention plugins to the `login:data` build configuration.
    - Upgrade project-wide language level and bytecode target from JDK 11 to JDK 21.
    - Relocate Kotlin source files from `src/main/java` to `src/main/kotlin` within the `login:data` module.
    - Downgrade Kotlin JPS plugin version from 2.3.20 to 2.2.21.
    - Define the Android namespace for the `login:data` module and remove explicit JVM target 11 configurations.

--- login/data/build.gradle.kts
+++ login/data/build.gradle.kts
@@ -1,13 +1,15 @@
 plugins {
-    id("java-library")
-    alias(libs.plugins.jetbrains.kotlin.jvm)
+    libs.plugins.run {
+        alias(library)
+        alias(di)
+    }
 }
-java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+
+android {
+    namespace = "ir.rahim.login.data"
 }
-kotlin {
-    compilerOptions {
-        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
+dependencies {
+    projects.run {
+
     }
 }

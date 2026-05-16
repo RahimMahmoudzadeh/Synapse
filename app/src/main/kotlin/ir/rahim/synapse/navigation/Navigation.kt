0014c8784413237c955f6ea5563710e8ef168987
Rahim Mahmoudzadeh <59431205+RahimMahmoudzadeh@users.noreply.github.com>
2026-05-16 06:21:34 +0330

    Implement serializable Navigation 3 backstack and update screen definitions
    - Update `Screens` sealed interface to implement `NavKey` and add the `@Serializable` annotation.
    - Add `Home` screen destination to the `Screens` interface.
    - Implement `rememberSynapseNavBackStack` helper function using `rememberSerializable` for backstack persistence.
    - Refactor `NavDisplay` to utilize `NavBackStack` for navigation state management instead of `mutableStateListOf`.

--- app/src/main/kotlin/ir/rahim/synapse/navigation/Navigation.kt
+++ app/src/main/kotlin/ir/rahim/synapse/navigation/Navigation.kt
@@ -1,16 +1,18 @@
 package ir.rahim.synapse.navigation
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSerializable
 import androidx.compose.ui.Modifier
+import androidx.navigation3.runtime.NavBackStack
 import androidx.navigation3.runtime.entryProvider
+import androidx.navigation3.runtime.rememberNavBackStack
 import androidx.navigation3.ui.NavDisplay
 import ir.rahim.library.navigation.screens.Screens
+import kotlinx.serialization.serializer
 
 @Composable
 fun NavDisplay(modifier: Modifier = Modifier) {
-    val backStack = remember { mutableStateListOf<Screens>(Screens.Login) }
+    val backStack = rememberSynapseNavBackStack(Screens.Login)
 
     NavDisplay(
         modifier = modifier,
@@ -18,8 +20,14 @@ fun NavDisplay(modifier: Modifier = Modifier) {
         onBack = { backStack.removeLastOrNull() },
         entryProvider = entryProvider {
             entry<Screens.Login> {
-
             }
         }
     )
+}
+
+@Composable
+fun rememberSynapseNavBackStack(vararg elements: Screens): NavBackStack<Screens> {
+    return rememberSerializable(serializer = serializer()) {
+        NavBackStack(*elements)
+    }
 }
--- library/navigation/src/main/kotlin/ir/rahim/library/navigation/screens/Screens.kt
+++ library/navigation/src/main/kotlin/ir/rahim/library/navigation/screens/Screens.kt
@@ -1,6 +1,11 @@
 package ir.rahim.library.navigation.screens
 
+import androidx.navigation3.runtime.NavKey
+import kotlinx.serialization.Serializable
 
-sealed interface Screens {
+
+@Serializable
+sealed interface Screens: NavKey {
     data object Login: Screens
+    data object Home: Screens
 }
